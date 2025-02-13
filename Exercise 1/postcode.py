853a45b49944b7f697031c06bbbc8af76b56e19d
vaishnav-idk <vaishnav_chandran@yahoo.com>
2025-02-14 04:14:13 +0530

    made the pincode regex code into a seperate function

--- Exercise 1/postcode.py
+++ Exercise 1/postcode.py
@@ -4,8 +4,11 @@ import json
 import re
 
 # import re for regex and json for handling api response
-
+def pincode_return(address):
+	pattern = r'\b\(d{6}\b'
+	match = re.search(pattern, address)
+	pincode = match.group(0)
+	return pincode
 def checK_valid_address(address):
-	pattern=r'\b\(d{6}\b'
-	match=re.search(pattern,address)
-	pincode
+	pincode = pincode_return(address)
+	url=f''
