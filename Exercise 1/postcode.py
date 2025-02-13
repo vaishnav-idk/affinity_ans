0a6252b86a326deaff53d7d405a0e80f2e674b75
vaishnav-idk <vaishnav_chandran@yahoo.com>
2025-02-14 04:12:08 +0530

    rewrite the entire codebase an coherent modular code

--- Exercise 1/postcode.py
+++ Exercise 1/postcode.py
@@ -1,51 +1,11 @@
 #as we are dealing with apis on python , we should use the requests package
-import json
-
 import requests as req
+import json
 import re
 
-#using regex patter for finding pincode
-pattern=r'\b(\d{6})\b'
-address="2nd Phase, 374/B, 80 Feet Rd, Mysore Bank Colony,Banashankari 3rd Stage, Srinivasa Nagar, Bengaluru, Karnataka 560050"
-match=re.search(pattern,address)
-pincode=match.group(1)
-print(pincode)
+# import re for regex and json for handling api response
 
-
-
-api_url="https://api.postalpincode.in/pincode/560050"
-api_response=req.get(api_url)
-data=api_response.json()
-if api_response.status_code !=200:
-		print("error")
-else:
-
-		with open("api_data.json","w") as file:
-			json.dump(data,file,indent=4)
-			file.close()
-
-postoffices=data[0]["PostOffice"]
-
-for postoffice in postoffices:
-	if(postoffice["State"].lower() in address.lower() or
-		postoffice["District"].lower() in address.lower() or
-		postoffice["Name"].lower() in address.lower()):
-			print("valid address for pincode")
-
-#considering the various names that each district and place using a dictonairy
-def get_actual_place(address):
-	std_address=""
-	dict={
-		"bengaluru" : "bangalore",
-		"mysore bank colony" : "state bank of mysore colony"
-	}
-	place = re.split(r",\s*|\.\s*", address)
-	for i in place:
-		i=i.lower()
-		if i in dict:
-			std_address=std_address+" "+dict[i]
-		else:
-			std_address=std_address+" "+i
-
-	print(std_address)
-get_actual_place(address)
+def checK_valid_address(address):
+	pattern=r'\b\(d{6}\b'
+	match=re.search(pattern,address)
+	pincode
