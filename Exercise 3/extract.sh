1a2eeeeababded2c9732f554dd8a31e66b760ce6
vaishnav-idk <vaishnav_chandran@yahoo.com>
2025-02-13 21:49:08 +0530

    change file name in awk command

--- Exercise 3/extract.sh
+++ Exercise 3/extract.sh
@@ -5,4 +5,4 @@ curl https://www.amfiindia.com/spages/NAVAll.txt > nava.txt
 
 #using awk to extract data from txt file as columns
 
-awk -F ';' '{print $4 $5}' nav.txt
+awk -F ';' '{print $4 $5}' nava.txt > 
