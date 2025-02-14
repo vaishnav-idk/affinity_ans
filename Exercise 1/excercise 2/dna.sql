965bbc9d83c5bdf96462fdf5b771f61ea281236f
vaishnav-idk <vaishnav_chandran@yahoo.com>
2025-02-14 10:28:35 +0530

    mistake is writing down pattern for like operator corrected

--- Exercise 1/excercise 2/dna.sql
+++ Exercise 1/excercise 2/dna.sql
@@ -3,13 +3,13 @@ Question 1
 asked to find the number of number of tigers part of the taxonomy database
  */
 
- select * from taxonomy where species like '%Panthera tigris%';
+ select * from taxonomy where species like 'Panthera tigris%';
 
  /* hint was to use scientific name of tigers however directly using the name showed an empty set because of the subspieces labled using baracket used like pattern match*/
 
  --to get count of the number of rows we use count aggregate function
 
- select count(*) from taxonomy where species like '%Panthera tigris%'
+ select count(*) from taxonomy where species like 'Panthera tigris%'
 
  /*answer was 8
  +----------+
@@ -19,3 +19,27 @@ asked to find the number of number of tigers part of the taxonomy database
 +----------+
 */
 
+/* Question 2 
+what is the ncb_id of the Sumatran tiger
+*/
+--the scientfic names was found from earlie select query 
+mysql> select ncbi_id,species from taxonomy where species like '%Panthera tigris sumatrae%';
+/*
++---------+-------------------------------------------+
+| ncbi_id | species                                   |
++---------+-------------------------------------------+
+|    9695 | Panthera tigris sumatrae (Sumatran tiger) |
++---------+-------------------------------------------+
+1 row in set (0.65 sec)
+*/
+
+
+/* 
+Question 3
+ Find all the columns that can be used to connect the tables in the given database.
+
+ Answer: * rafam_acc and clan_acc connected via clan membership table
+		* rfam_acc and rfamseq_acc connected via full full region table
+		* ncbi_id in both rframdseq and taxonomy tables
+
+ */
